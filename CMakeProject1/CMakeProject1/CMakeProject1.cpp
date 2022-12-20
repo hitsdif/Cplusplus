fb15b85f70eda8804ed6a75db44b849fb4f96d96
Svttam <hhh829370@gmail.com>
2022-12-20 17:00:26 +0300

    Cplusplus

---
+++ CMakeProject1/CMakeProject1/CMakeProject1.cpp
@@ -0,0 +1,14 @@
+﻿// CMakeProject1.cpp: определяет точку входа для приложения.
+//
+
+#include "CMakeProject1.h"
+#include "iostream"
+
+using namespace std;
+
+int main()
+{
+	string str = "Hello wold";
+	cout << str << endl;
+	return 0;
+}
